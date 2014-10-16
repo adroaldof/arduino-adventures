f8a4b9e6e237be8b85ef07397ac95779d07de466
Adroaldo de Andrade <adroaldof@gmail.com>
2014-10-16 12:39:06 -0300

    Update blink for tests

--- blink/blink.ino
+++ blink/blink.ino
@@ -6,21 +6,23 @@ int ledPin = 13;
 void setup () {
     // Initialize digital pin as output
     pinMode(ledPin, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop () {
+  Serial.println("Blink loop");
   digitalWrite(ledPin, HIGH);   // Turn led on
   delay(200);                   // Wait 200 milliseconds
   digitalWrite(ledPin, LOW);    // Turn led off
   delay(200);                   // Wait 200 milliseconds
 
   digitalWrite(ledPin, HIGH);
-  delay(50);
+  delay(200);
   digitalWrite(ledPin, LOW);
-  delay(50);
+  delay(200);
 
   digitalWrite(ledPin, HIGH);
-  delay(100);
+  delay(200);
   digitalWrite(ledPin, LOW);
-  delay(3000);
+  delay(2000);
 }
